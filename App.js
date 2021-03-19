510f3e78d1b5860b717fbe13034f8df8e9576e8b
rmiglin <rossmiglin@gmail.com>
2021-03-19 05:22:38 -0400

    create register page

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 
+import { View, Text } from 'react-native'
 import * as firebase from 'firebase'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import LandingScreen from './components/auth/Landing';
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDG-F-CndYAbSwvBcCWrpbKChBfZHZ6cko",
@@ -22,14 +24,57 @@ if(firebase.apps.length === 0){
 }
 
 const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator intitialRouteName="Landing">
-        <Stack.Screen name="Landing" component={ LandingScreen } options={{ headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
 
-  )
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+    if(!loggedIn){
+      return (
+        <NavigationContainer>
+          <Stack.Navigator intitialRouteName="Landing">
+            <Stack.Screen name="Landing" component={ LandingScreen } options={{ headerShown: false}}/>
+            <Stack.Screen name="Register" component={RegisterScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+
+      );
+    }
+
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>User is logged in</Text>
+      </View>
+    )
+  }
 }
 
--- components/auth/Login.js
+++ components/auth/Login.js
@@ -3,22 +3,21 @@ import { View, Button, TextInput } from 'react-native'
 
 import firebase from 'firebase'
 
-export class Register extends Component {
+export class Login extends Component {
     constructor(props){
         super(props);
 
         this.state =  {
             email: '',
             password: '',
-            name: ''
         }
 
         this.onSignUp = this.onSignUp.bind(this)
     }
 
     onSignUp(){
-        const { email, password, name } = this.state;
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password)
         .then((result) => {
             console.log(result)
         })
@@ -31,10 +30,6 @@ export class Register extends Component {
         return (
             <View>
                 <TextInput
-                    placeholder="name"
-                    onChangeText={(name) => this.setState({ name })}
-                />
-                <TextInput
                     placeholder="email"
                     onChangeText={(email) => this.setState({ email })}
                 />
@@ -45,12 +40,11 @@ export class Register extends Component {
                 />
                 <Button
                     onPress={() => this.onSignUp()}
-                    title="Sign Up"
+                    title="Sign In"
                 />
             </View>
         )
     }
 }
 
-export default Register
-
+export default Login
