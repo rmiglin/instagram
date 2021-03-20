4050e9e8c7ffdf8e6655dd28ac094943f8c8439b
rmiglin <rossmiglin@gmail.com>
2021-03-20 19:34:16 -0400

    create state management for users

--- App.js
+++ App.js
@@ -8,6 +8,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
+import MainScreen from './components/Main'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDG-F-CndYAbSwvBcCWrpbKChBfZHZ6cko",
@@ -70,9 +77,9 @@ export class App extends Component {
     	}
 
     	return (
-      		<View style={{ flex: 1, justifyContent: 'center' }}>
-        		<Text>User is logged in</Text>
-      		</View>
+			<Provider store = { store }>
+				<MainScreen/>
+			</Provider>
     	)
   	}
 }
--- components/auth/Register.js
+++ components/auth/Register.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { View, Button, TextInput } from 'react-native'
 
 import firebase from 'firebase'
+import 'firebase/firestore'
 
 export class Register extends Component {
     constructor(props){
@@ -22,6 +23,10 @@ export class Register extends Component {
             .then((result) => {
                 firebase.firestore().collection("users")
                     .doc(firebase.auth().currentUser.uid)
+                    .set({
+                        name,
+                        email
+                    })
                 console.log(result)
             })
             .catch((error) => {
