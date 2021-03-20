4e7218e88510ef07a43e73e2c9fe72e4ce3e06ed
rmiglin <rossmiglin@gmail.com>
2021-03-19 22:41:59 -0400

    update register to connect with firebase auth

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -19,12 +19,14 @@ export class Register extends Component {
     onSignUp(){
         const { email, password, name } = this.state;
         firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((result) => {
-            console.log(result)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+            .then((result) => {
+                firebase.firestore().collection("users")
+                    .doc(firebase.auth().currentUser.uid)
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
 
     render() {
