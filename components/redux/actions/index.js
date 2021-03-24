c69590f2ea66cde1c0b0f0dacbc901db6df6a0ae
rmiglin <rossmiglin@gmail.com>
2021-03-24 02:04:51 -0400

    update firestore database name typo

--- components/redux/actions/index.js
+++ components/redux/actions/index.js
@@ -1,17 +1,17 @@
 import { USER_STATE_CHANGE } from '../constants/index'
 import firebase from 'firebase'
-import 'firebase/firestore'
+require('firebase/firestore')
 
 export function fetchUser(){
     return((dispatch) => {
         firebase.firestore()
-            .collection("user")
+            .collection("users")
             .doc(firebase.auth().currentUser.uid)
             .get()
             .then((snapshot) => {
-                if(snapshot.exists){
+                if (snapshot.exists) {
                     console.log(snapshot.data())
-                    dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data()})
+                    dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() })
                 } else {
                     console.log("does not exist")
                 }
