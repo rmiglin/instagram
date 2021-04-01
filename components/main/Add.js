d314be0c51a89577c2a9c050333c48bb047c9735
rmiglin <rossmiglin@gmail.com>
2021-04-01 01:50:16 -0400

    add take photo button

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
@@ -13,6 +14,13 @@ export default function App() {
     })();
   }, []);
 
+  const takePicture = async () => {
+    if(camera){
+        const data = await camera.takePictureAsync(null)
+        console.log(data.uri)
+    }
+  }
+
   if (hasPermission === null) {
     return <View />;
   }
@@ -20,22 +28,27 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
+    <View style={{ flex: 1 }}>
+        <View style={ styles.cameraContainer }>
+        <Camera 
+            ref={ref => setCamera(ref)}
+            style={ styles.fixedRatio } 
+            type={type}
+            ratio={'1:1'}/>
+        </View>
+
+        <Button
+            title="Flip Image"
             onPress={() => {
-              setType(
+                setType(
                 type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
             }}>
             <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+        </Button>
+        <Button title="Take Picture" onPress={() => takePicture()}/>
     </View>
   );
 }
